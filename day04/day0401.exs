5aee7eb9a4453955f128bdb1f5e38d8c3faf049d
Andy Gonçalves <agoncalves1@gmail.com>
2021-12-04 18:50:38 +0000

    Add day 4, part 1

---
+++ day04/day0401.exs
@@ -0,0 +1,60 @@
+winning_board? = fn board, x, y ->
+  values = Map.values(board)
+
+  if Enum.filter(values, fn {tx, _} -> tx == x end) |> Enum.empty?() do
+    true
+  else
+    Enum.filter(values, fn {_, ty} -> ty == y end) |> Enum.empty?()
+  end
+end
+
+count_points = fn board, draw ->
+  board_points =
+    Map.keys(board)
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.sum()
+
+  (board_points * String.to_integer(draw))
+  |> IO.inspect()
+end
+
+file_pid = File.open!("input.txt")
+
+draws = IO.read(file_pid, :line) |> String.trim() |> String.split(",")
+
+boards =
+  IO.stream(file_pid, :line)
+  |> Stream.chunk_every(6)
+  |> Enum.reduce([], fn ["\n" | row_strings_list], boards ->
+    {board, _} =
+      Stream.flat_map(row_strings_list, &String.split/1)
+      |> Enum.reduce({[], 0}, fn number, {acc, i} ->
+        {[{number, {div(i, 5), rem(i, 5)}} | acc], i + 1}
+      end)
+
+    [board | boards]
+  end)
+  |> Enum.map(&Map.new/1)
+
+Enum.reduce_while(draws, boards, fn draw, boards ->
+  new_boards =
+    Enum.map(boards, fn board ->
+      case Map.pop(board, draw) do
+        {nil, _} ->
+          board
+
+        {{x, y}, new_board} ->
+          if winning_board?.(new_board, x, y) do
+            count_points.(new_board, draw)
+          else
+            new_board
+          end
+      end
+    end)
+
+  if Enum.any?(new_boards, &Kernel.is_integer/1) do
+    {:halt, new_boards}
+  else
+    {:cont, new_boards}
+  end
+end)
