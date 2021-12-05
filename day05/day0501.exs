462544391631ee7b8a837062998b911734d4823a
Andy Gonçalves <agoncalves1@gmail.com>
2021-12-05 12:11:59 +0000

    Add day 5, part 1

---
+++ day05/day0501.exs
@@ -0,0 +1,28 @@
+defmodule LinesAndPoints do
+  def line_points([{x, y1}, {x, y2}]) do
+    Enum.map(y1..y2, fn y -> {x, y} end)
+  end
+
+  def line_points([{x1, y}, {x2, y}]) do
+    Enum.map(x1..x2, fn x -> {x, y} end)
+  end
+
+  # diagonals not considered for this part
+  def line_points([{x1, y1}, {x2, y2}]) do
+    []
+  end
+end
+
+File.stream!("input.txt")
+|> Stream.map(fn line ->
+  String.trim(line)
+  |> String.split(" -> ")
+  |> Enum.map(fn pair ->
+    String.split(pair, ",") |> Enum.map(&String.to_integer/1) |> List.to_tuple()
+  end)
+end)
+|> Stream.flat_map(&LinesAndPoints.line_points/1)
+|> Enum.frequencies()
+|> Stream.reject(fn {_, freq} -> freq == 1 end)
+|> Enum.count()
+|> IO.inspect()
