0b384fef5473296cd5fe0739789e6383bab6f54a
Andy Gonçalves <agoncalves1@gmail.com>
2021-12-07 14:50:35 +0000

    Add day 7

---
+++ day07/day0701.exs
@@ -0,0 +1,23 @@
+crabs =
+  File.open!("input.txt")
+  |> IO.read(:line)
+  |> String.trim()
+  |> String.split(",")
+  |> Stream.map(&String.to_integer/1)
+
+min_pos = Enum.min(crabs)
+max_pos = Enum.max(crabs)
+crabs_per_position = Enum.frequencies(crabs)
+
+distances = Enum.map(min_pos..max_pos, fn pos -> {pos, 0} end)
+
+Enum.reduce(crabs_per_position, distances, fn {from, number_of_crabs}, distances ->
+  updated_distances =
+    Enum.map(distances, fn {to, fuel} ->
+      {to, fuel + abs(from - to) * number_of_crabs}
+    end)
+
+  updated_distances
+end)
+|> Enum.min(fn {_, x}, {_, y} -> x < y end)
+|> IO.inspect()
